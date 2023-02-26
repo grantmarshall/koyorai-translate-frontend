c877f67d77f02471e72e462d09a819c1be219549
Grant Marshall <gandrewmarshall@gmail.com>
2023-02-25 20:31:16 -0700

    Clean up recorder API and remove reset button for now. All GET/POST requests appear to be in order for actually saving to db now.

--- koyorai/static/translate/app.js
+++ koyorai/static/translate/app.js
@@ -1,15 +1,6 @@
-// Helper function for generating UUIDs
-function uuidv4() {
-    return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
-        (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
-    );
-}
-
 // Object for controlling the actual recording of audio chunks
 var audioRecorder = {
-    audioStream: null,
     mediaRecorder: null,
-    chunks: [],
     // Start recording audio from the user's mic and execute the provided
     // callback on each data chunk recorded by the MediaRecorder.
     start: function (callback) {
@@ -20,15 +11,11 @@ var audioRecorder = {
                     'mediaDevices API or getUserMedia method is not supported in this browser.'));
         }
         return navigator.mediaDevices.getUserMedia({ audio: true }).then((stream) => {
-            audioRecorder.audioStream = stream;
-
             // Create a MediaRecorder instance that will trigger the data
             // callback every 100ms.
             audioRecorder.mediaRecorder = new MediaRecorder(stream);
             audioRecorder.mediaRecorder.ondataavailable = function(event) {
-                console.log('Audio chunk recorded');
                 callback(event.data);
-                audioRecorder.chunks.push(event.data);
             };
             audioRecorder.mediaRecorder.start(500);
         });
@@ -36,56 +23,22 @@ var audioRecorder = {
     // Pause recording audio from the user's mic
     stop: function () {
         audioRecorder.mediaRecorder.stop();
-        chunks = [];
-        return new Promise((resolve) => {
-            console.log('Stopping audio api');
-            resolve(1);
-        });
     },
-    // Reset the audio being sent from the user and create a new session
-    cancel: function () {
-        return new Promise((resolve) => {
-            console.log('Cancelling audio api');
-            resolve(1);
-        });
-    }
 };
 
 // Node whose innertext will contain the translations
 var outputField = document.querySelector('.output-field');
 
 // Logic for session management
-var sessionId;
-var sessionStartTs;
 var startSession = function() {
-    sessionId = uuidv4();
-    sessionStartTs = (new Date()).getTime();
-    fetch('http://127.0.0.1:5000/translate/start_session', {
-        method: 'POST',
+    return fetch('http://127.0.0.1:5000/translate/session?timestamp=' + (new Date()).getTime(), {
+        method: 'GET',
         headers: {
             'Accept': 'application/json',
             'Content-Type': 'application/json'
-        },
-        // Set the post data
-        body: JSON.stringify({
-            id: sessionId,
-            startTs: sessionStartTs
-        })
-    }).then((response) => {
-        console.log(
-            'Session with id %s started at timestamp %s with status %s',
-            sessionId,
-            sessionStartTs,
-            response.message);
-        outputField.innerHTML = 'Awaiting translation...';
-    }).except((error) => {
-        console.log('Failed to start session with error %s', error);
+        }
     })
 };
-var endSession = function() {
-    sessionId = null;
-    sessionStartTs = null;
-}
 
 // Logic for record button
 var recordButton = document.querySelector('.record-button');
@@ -93,60 +46,47 @@ var recordButtonState = {
     recording: false
 }
 recordButton.onclick = function () {
-    if (recordButtonState.recording) {
-        audioRecorder.stop().then(() => {
-            console.log('Pausing audio recording');
-            recordButton.innerHTML = 'Start';
-        });
+    if (audioRecorder.mediaRecorder && audioRecorder.mediaRecorder.state == 'recording') {
+        audioRecorder.stop();
+        recordButton.innerHTML = 'Start';
     } else {
-        audioRecorder.start(data => {
-            data.text().then(dataString => {
-                // Callback to send data to the server with each chunk
-                fetch('http://127.0.0.1:5000/translate/update', {
-                    method: 'POST',
-                    headers: {
-                        'Accept': 'application/json',
-                        'Content-Type': 'application/json'
-                    },
-                    // Set the post data
-                    body: JSON.stringify({
-                        id: sessionId,
-                        type: data.type,
-                        size: data.size,
-                        timestamp: (new Date()).getTime(),
-                        data: dataString
-                    })
-                }).then(response => console.log(response));
-            });
-        }).then(() => {
-            // Ran when the audio stream successfully starts
-            console.log('Starting audio recording');
-
-            // If we don't have an active session start a new one
-            if (sessionId == null) {
-                startSession();
-            }
-
-            recordButton.innerHTML = 'Stop';
-        })
-        .catch(error => {
+        startSession().then((response) => { return response.json(); }).then((response) => {
+            var sessionId = response.sessionId;
+            var sessionStartTs = response.sessionStartTs;
+            audioRecorder.start(data => {
+                data.text().then(dataString => {
+                    // Callback to send data to the server with each chunk
+                    fetch('http://127.0.0.1:5000/translate/translation', {
+                        method: 'POST',
+                        headers: {
+                            'Accept': 'application/json',
+                            'Content-Type': 'application/json'
+                        },
+                        // Set the post data
+                        body: JSON.stringify({
+                            id: sessionId,
+                            type: data.type,
+                            size: data.size,
+                            timestamp: (new Date()).getTime() - sessionStartTs,
+                            data: dataString
+                        })
+                    }).then(response => console.log(response));
+                });
+            }).then(() => {
+                // Ran when the audio stream successfully starts
+                console.log('Starting audio recording');
+                recordButton.innerHTML = 'Stop';
+            })
+        }).catch((error) => {
             // No Browser Support Error
             if (error.message.includes("mediaDevices API or getUserMedia method is not supported in this browser.")) {       
                 console.log("To record audio, use browsers like Chrome and Firefox.");
+                return;
+            } else {
+                console.log(error);
                 return;
             }
         });
     }
     recordButtonState.recording = !recordButtonState.recording;
-}
-
-// Logic for reset button
-var resetButton = document.querySelector('.reset-button');
-resetButton.onclick = function () {
-    // Make sure we're not going to send any more chunks
-    if (recordButtonState.recording) {
-        recordButton.click();
-    }
-    // End the current session
-    endSession();
 }
--- koyorai/translate.py
+++ koyorai/translate.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
+from uuid import uuid4
 
 bp = Blueprint('translate', __name__, url_prefix='/translate')
 
@@ -9,13 +10,25 @@ def app():
     return render_template('translate.html')
 
 
-@bp.route('/update', methods=['GET', 'POST'])
+@bp.route('/translation', methods=['GET', 'POST'])
 def update():
     """Endpoint to post audio data for tl or get most recent tl in session."""
-    return {'message': 'not implemented'}, 501
+    if request.method == 'GET':
+        return {}, 200
+    else:
+        print(request.json)
+        return {}, 200
 
 
-@bp.route('/start_session', methods=['POST'])
+@bp.route('/session', methods=['GET'])
 def start_session():
     """Endpoint to start a tl session."""
-    return {'message': 'not implemented'}, 501
+    uuid = uuid4()
+    timestamp = request.args.get('timestamp')
+
+    # TODO(grantmarshall): Implement the insertion logic for a new session
+
+    return {
+        'sessionId': uuid,
+        'sessionStartTs': timestamp
+    }, 200
